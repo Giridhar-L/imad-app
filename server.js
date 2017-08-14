983671df02595b3d75fb50a21cc0ea71b8a765e1
Giridhar <giridharprasad2897@gmail.com>
2017-08-15 00:35:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.get('/article-one', function (req, res) {
   res.send(createTemplate(datum));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter +1;
+    res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
