d48bf285cb604deb4d6d2917cba21220f8a893fc
Giridhar <giridharprasad2897@gmail.com>
2017-08-17 00:54:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,7 @@ document.getElementById('btnid').onclick= function(){
     
 
     var list = ['name','fuck'];
-    var text='';
+    var text=``;
     for(var i=0;i<list.length;i++)
     {
         text+='<li>'+list[i]+'</li>';
